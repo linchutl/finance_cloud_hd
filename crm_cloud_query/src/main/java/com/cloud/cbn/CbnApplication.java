a98ee7e4dbe3230992d40511b068d3268734710d
hul <htl123qwe>
2022-12-03 17:09:34 +0800

    1203

---
+++ crm_cloud_query/src/main/java/com/cloud/cbn/CbnApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.cbn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbnApplication.class, args);
+	}
+
+}
